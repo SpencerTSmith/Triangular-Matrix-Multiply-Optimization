2e2df7f24f74c2a79b3bdeea1f74030567d20b8e
SpencerTSmith <spencertsmith2003@gmail.com>
2024-11-20 22:17:52 -0600

    add all gpu variants... probably should have split this into multiple commits... oops

--- GPU/op_dispatch_vars.sh
+++ GPU/op_dispatch_vars.sh
@@ -9,14 +9,14 @@ OP_BASELINE_FILE="./src/baseline_op.c"    #
 # HOWEVER, CHANGE THESE LINES:             #
 # Replace the filenames with your variants #
 ############################################
-OP_SUBMISSION_VAR01_FILE="./src/tuned_variant01_op.c"
-OP_SUBMISSION_VAR01_CUDA="./src/tuned_variant01_op.cu"
+OP_SUBMISSION_VAR01_FILE="./src/good_cuda.c"
+OP_SUBMISSION_VAR01_CUDA="./src/good_cuda.cu"
 
-OP_SUBMISSION_VAR02_FILE="./src/tuned_variant02_op.c"
-OP_SUBMISSION_VAR02_CUDA="./src/tuned_variant02_op.cu"
+OP_SUBMISSION_VAR02_FILE="./src/gooder_cuda.c"
+OP_SUBMISSION_VAR02_CUDA="./src/gooder_cuda.cu"
 
-OP_SUBMISSION_VAR03_FILE="./src/tuned_variant03_op.c"
-OP_SUBMISSION_VAR03_CUDA="./src/tuned_variant03_op.cu"
+OP_SUBMISSION_VAR03_FILE="./src/as_good_cuda.c"
+OP_SUBMISSION_VAR03_CUDA="./src/as_good_cuda.cu"
 
 ######################################################
 # You can even change the compiler flags if you want #
